38df938db5d54a4eac7925ad9e5037ca7d190aec
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 14:06:55 +0400

    accordion works

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,6 +1,7 @@
 import iconStar from "../../assets/images/icon-star.svg";
 import iconPlus from "../../assets/images/icon-plus.svg";
 import iconMinus from "../../assets/images/icon-minus.svg";
+import { useState } from "react";
 function Accordion() {
     const [activePanel, setActivePanel] = useState(null);
     const [panels, setPanels] = useState([
@@ -26,36 +27,38 @@ function Accordion() {
             content: `The best (and quickest) way to get help on a challenge is in our Discord server. There are thousands of other developers in there, so it's a great place to ask questions. We even have a dedicated "help" channel! If you haven't joined yet, you can get an invite to our Discord server here.`
         }
     ]);
+    function handleToggle(id) {
+        setActivePanel(activePanel === id ? null : id)
+    }
     return (
         <>
             <section className="main-faq">
                 <div className="faq-header">
                     <h1 className="flex"><img src={iconStar} alt="" />FAQs</h1>
                 </div>
-                <div class="faq-body">
+                <div className="faq-body">
+                    {/* {panels.map((panel) => (
                     <button class="accordion flex">What is Frontend Mentor, and how will it help me?<img
                         src={iconPlus} alt="" /></button>
                     <div class="panel">
                         <p>Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills
                             with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.</p>
                     </div>
+                    )}
+                </div> */}
+                    {panels.map((panel) => (
+                        <div key={panel.id} className={` ${activePanel === panel.id ? 'accordion-active' : ''}`} onClick={() => handleToggle(panel.id)}>
+                            <button className="accordion flex">What is Frontend Mentor, and how will it help me?<img
+                                src={iconPlus} alt="" className={activePanel === panel.id ? "active" : ""}/>
 
-                    <button class="accordion flex">Is Frontend Mentor free?<img
-                        src={iconPlus} alt="" /></button>
-                    <div class="panel">
-                        <p>The majority of our challenges are free, yes. We do have some that are premium and require a Pro subscription to access. It will say on each challenge whether they are free or premium, so it's easy to tell the difference.</p>
-                    </div>
-
-                    <button class="accordion flex">Can I use Frontend Mentor projects in my portfolio?<img
-                        src={iconPlus} alt="" /></button>
-                    <div class="panel">
-                        <p>Definitely! Please do feel free to use whatever you build in your portfolio. Helping developers add professional-looking projects to their portfolio was one of the reasons we created this platform!</p>
-                    </div>
-                    <button class="accordion flex">How can I get help if I'm stuck on a challenge?<img
-                        src={iconPlus} alt="" /></button>
-                    <div class="panel">
-                        <p>The best (and quickest) way to get help on a challenge is in our Discord server. There are thousands of other developers in there, so it's a great place to ask questions. We even have a dedicated "help" channel! If you haven't joined yet, you can get an invite to our Discord server here.</p>
-                    </div>
+                            </button>
+                            {activePanel === panel.id && (
+                                <div className="accordion-content">
+                                    <p>{panel.content}</p>
+                                </div>
+                            )}
+                        </div>
+                    ))}
                 </div>
             </section>
         </>
