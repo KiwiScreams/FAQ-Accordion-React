d5e24a7a148274dd55aaa74adc3b6776087654f9
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 13:45:43 +0400

    routes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
+import Accordion from "../../components/accordion/Accordion"
 import "./Home.css"
-function Home()
-{
-    return(
-        <></>
+function Home() {
+    return (
+        <>
+            <Accordion />
+        </>
     )
 }
 export default Home
