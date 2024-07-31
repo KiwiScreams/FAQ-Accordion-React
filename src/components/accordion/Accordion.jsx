dfaf93bed0949701c3b0f2a25718983fab147962
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 13:53:18 +0400

    header

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,8 +1,12 @@
-function Accordion()
-{
-    return(
+import iconStar from "../../assets/images/icon-star.svg"
+function Accordion() {
+    return (
         <>
-        
+            <section className="main-faq">
+                <div className="faq-header">
+                    <h1 className="flex"><img src={iconStar} alt=""/>FAQs</h1>
+                </div>
+            </section>
         </>
     )
 }
