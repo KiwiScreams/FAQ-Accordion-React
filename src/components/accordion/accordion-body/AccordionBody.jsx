40d6f54f63c3b398cd92f26e2e96e416b4c71d3f
KiwiScreams <screamskiwi3@gmail.com>
2024-07-31 14:52:42 +0400

    faq body

---
+++ src/components/accordion/accordion-body/AccordionBody.jsx
@@ -0,0 +1,59 @@
+import iconPlus from "../../../assets/images/icon-plus.svg";
+import iconMinus from "../../../assets/images/icon-minus.svg";
+import { useState } from "react";
+import "./AccordionBody.css";
+function AccordionBody() {
+    const [activePanel, setActivePanel] = useState(null);
+    const [panels, setPanels] = useState([
+        {
+            id: 1,
+            title: "What is Frontend Mentor, and how will it help me?",
+            content: `Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills
+                            with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.`
+        },
+        {
+            id: 2,
+            title: "Is Frontend Mentor free?",
+            content: `The majority of our challenges are free, yes. We do have some that are premium and require a Pro subscription to access. It will say on each challenge whether they are free or premium, so it's easy to tell the difference.`
+        },
+        {
+            id: 3,
+            title: "Can I use Frontend Mentor projects in my portfolio?",
+            content: `Definitely! Please do feel free to use whatever you build in your portfolio. Helping developers add professional-looking projects to their portfolio was one of the reasons we created this platform!`
+        },
+        {
+            id: 4,
+            title: "How can I get help if I'm stuck on a challenge?",
+            content: `The best (and quickest) way to get help on a challenge is in our Discord server. There are thousands of other developers in there, so it's a great place to ask questions. We even have a dedicated "help" channel! If you haven't joined yet, you can get an invite to our Discord server here.`
+        }
+    ]);
+    function handleToggle(id) {
+        setActivePanel(activePanel === id ? null : id)
+    }
+    return (
+        <>
+            <div className="faq-body">
+                {panels.map((panel) => (
+                    <div key={panel.id} className="faq" onClick={() => handleToggle(panel.id)}>
+                        <button className="accordion flex">
+                            {panel.title}
+                            <span className={activePanel === panel.id ? "active" : ""}>
+                                {activePanel === panel.id ? (
+                                    <img src={iconMinus} alt="" />
+                                ) : (
+                                    <img src={iconPlus} alt="" />
+                                )}
+                            </span>
+                        </button>
+                        {activePanel === panel.id && (
+                            <div className="panel">
+                                <p>{panel.content}</p>
+                            </div>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
+export default AccordionBody
